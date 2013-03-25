42d5afa90e95852f625724b7edb6453e53e9803d
Trevor Hartman <trevorhartman@gmail.com>
2013-03-24 19:36:13 -0700

    Build a few trees

--- src/sakura/core.clj
+++ src/sakura/core.clj
@@ -1,6 +1,60 @@
-(ns sakura.core)
+(ns sakura.core "Playing with trees")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Node [value children])
+
+(defn mk-tree
+  [nested-list] nested-list)
+
+;;; Some trees to play with
+
+(def Rosaceae
+  "Rosaceae (the rose family) are a medium-sized family of flowering plants,
+   including about 2830 species in 95 genera.
+   Taken from http://en.wikipedia.org/wiki/Rosaceae"
+  (mk-tree '[Rosaceae
+             Rosoideae
+             Filipendula
+             [Rosodae
+              Sanguisorbeae
+              Potentilleae
+              Colurieae]
+             Dryadoideae
+             [Amygdaloideae
+              Lyonothamnus
+              Amygdaleae
+              Sorbarieae
+              Spiraeeae
+              [Kerriodae
+               Kerrieae
+               Osmarioneae]
+              [Pyrodae
+               Gillenia
+               [Maleae
+                Kageneckia
+                Vauquelinia
+                Lindleya
+                Malinae]]]]))
+
+(def Family
+  "In human context, a family (from Latin: familia) is a group of people affiliated
+   by consanguinity, affinity, or co-residence."
+  (mk-tree '[Great Grandparent
+             [Great Uncle
+              [First cousin once removed
+               [Second cousin
+                Second cousin once removed]]]
+             Great Aunt
+             [Grandparent
+              [Aunt
+               Cousin
+               Cousin]
+              [Uncle
+               Cousin]
+              [Parent
+               Self ; <-- You are Here
+               Brother
+               Sister]]]))
+
+
+
+(defn -main [])
